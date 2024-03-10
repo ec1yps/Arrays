609fc8db784d5d8e38fa8063d30d92beb742f834
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-03-10 16:44:59 +0300

    First HOMEWORK done

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+
+//#define EXAMPLE
+#define HOMEWORK
 
 void main() {
 	setlocale(LC_ALL, "");
+
+#ifdef EXAMPLE
 	const int SIZE = 5;
 	int arr[SIZE];
 
@@ -13,4 +18,38 @@ void main() {
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+#endif // EXAMPLE
+
+#ifdef HOMEWORK
+	const int SIZE = 5;
+	int arr[SIZE], result = 0, min = 1000000, max = 0;
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << "Введите " << i + 1 << "-й элемент массива: "; cin >> arr[i];
+	}
+	cout << "\nЭлементы массива в прямом порядке: ";
+
+	for (int i = 0; i < SIZE; i++)
+		cout << "\t" << arr[i];
+	cout << "\n\nЭлементы массива в обратном порядке: ";
+
+	for (int i = SIZE - 1; i >= 0; i--)
+		cout << "\t" << arr[i];
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++)
+		result += arr[i];
+	cout << "\nСумма элементов массива: " << result << endl;
+
+	cout << "\nСреднее арифметическое значение элементов массива: " << result / SIZE << endl;
+
+	for (int i = 0; i < SIZE; i++)
+		if (arr[i] < min) min = arr[i];
+	cout << "\nМинимальный элемент массива: " << min << endl;
+
+	for (int i = 0; i < SIZE; i++)
+		if (arr[i] > max) max = arr[i];
+	cout << "\nМаксимальный элемент массива: " << max << endl;
+#endif // HOMEWORK
+
 }
