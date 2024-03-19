af3811c151ddc472792a659eb39976512fa3e3b9
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-03-19 21:09:51 +0300

    ShiftRight home work DONE

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 #define tab "\t"
 
+//#define SHIFT_LEFT
+#define SHIFT_RIGHT
+
 void main() {
 	setlocale(LC_ALL, "");
 	const int n = 10;
 	int arr[n] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
 
+#ifdef SHIFT_LEFT
 	cout << "Исходный массив: " << endl;
 	for (int i = 0; i < n; i++) {
 		cout << arr[i] << tab;
@@ -29,4 +33,31 @@ void main() {
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+#endif // SHIFT_LEFT
+
+#ifdef SHIFT_RIGHT
+		cout << "Исходный массив: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	int number_of_shifts;
+	cout << "\nВведите число сдвигов: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++) {
+		int buffer = arr[n - 1];
+		for (int j = n - 1; j >= 0; j--) {
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = buffer;
+	}
+
+	cout << "\nСдвинутый массив: " << endl;
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // SHIFT_RIGHT
+
+
 }
