a0fbd676d49c245087e05fbafd76fe0050c2e72e
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-03-28 18:02:19 +0300

    DEC_2_HEX DONE

---
+++ Numerics/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define DEC_2_BIN
+#define DEC_2_HEX
+
+void main() {
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+
+#ifdef DEC_2_BIN
+	const int MAX_CAPASITY = 32;
+	bool binary[MAX_CAPASITY] = {};
+
+	int i = 0;
+	/*for (; decimal > 0; i++) {
+		binary[i] = decimal % 2;
+		decimal /= 2;
+	}*/
+	for (; decimal; decimal /= 2) binary[i++] = decimal % 2;
+
+	for (--i; i >= 0; i--) {
+		cout << binary[i];
+	}
+	cout << endl;
+#endif // DEC_2_BIN
+
+#ifdef DEC_2_HEX
+	const int MAX_HEX_CAPASITY = 8;
+	char hex[MAX_HEX_CAPASITY] = {};
+
+	int i = 0;
+	for (; decimal; i++) {
+		hex[i] = decimal % 16;
+		decimal /= 16;
+	}
+
+	for (--i; i >= 0; i--) {
+		/*if (hex[i] < 10)
+			cout << (int)hex[i];
+		else 
+			cout << char(hex[i] + 55);*/
+		cout << (hex[i] < 10 ? char(hex[i] + 48) : char(hex[i] + 55));
+	}
+	cout << endl;
+#endif // DEC_2_HEX
+
+}
